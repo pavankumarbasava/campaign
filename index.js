b8a54c6478e7ae293f2557f6a93f054c1072ad89
Pavan Basava <pavanbasava@MacBook-Air-M1.local>
2023-05-24 22:28:11 +0530

    second commit

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 const express= require('express');
-const app = express();
+ var passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys=require('./config/keys');
 
-app.get('/',(req,res,next)=>{
-    res.send({hi:'pavan'});
-})
-const PORT =process.env.PORT||5000;
+const app = express();
+passport.use(
+    new GoogleStrategy(
+    {
+    clientID:keys.googleClientId,
+    clientSecret:keys.googleSecreteKey,
+    callbackURL: '/auth/google/callback'
+}, 
+(accessToken,refreshToken,profile,done)=>{
+    console.log("access token "+accessToken);
+    console.log("refresh token "+refreshToken);
+    console.log("profile "+profile);
+}
+)); 
+
+app.get('/auth/google', passport.authenticate('google',{
+    //ask google to access profile and emails
+    scope:['profile','email']
+}))
+app.use('/auth/google/callback', passport.authenticate('google')
+)
+const PORT =process.env.PORT||3000;
 app.listen(PORT);
